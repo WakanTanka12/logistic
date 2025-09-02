1d79e9abf051276e76f0989b420f47948773e2dd
WakanTanka12 <samuelitorochav536@gmail.com>
2025-09-02 13:52:03 -0400

    DTOs
    Los DTOs ahora están completos

--- src/main/java/com/app/logistica/dtos/CustomerDTO.java
+++ src/main/java/com/app/logistica/dtos/CustomerDTO.java
@@ -1,4 +1,18 @@
 package com.app.logistica.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class CustomerDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
 }
--- src/main/java/com/app/logistica/mapperdtos/CustomerMapper.java
+++ src/main/java/com/app/logistica/mapperdtos/CustomerMapper.java
@@ -1,4 +1,7 @@
 package com.app.logistica.mapperdtos;
-@
+
+import com.app.logistica.dtos.CustomerDTO;
+import com.app.logistica.entities.Customer;@
 public class CustomerMapper {
+    public static CustomerDTO toDTO(Customer customer) {}
 }
